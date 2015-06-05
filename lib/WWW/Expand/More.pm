3799d90345f08238360799462d958e33003ce475
bayashi <bayashi@cpan.org>
2015-06-06 03:35:11 +0900

    just work

--- lib/WWW/Expand/More.pm
+++ lib/WWW/Expand/More.pm
@@ -1,15 +1,71 @@
 package WWW::Expand::More;
 use strict;
 use warnings;
-use Carp qw/croak/;
+
+use Furl;
 
 our $VERSION = '0.01';
 
-sub new {
-    my $class = shift;
-    my $args  = shift || +{};
+sub _default_ua {
+    my $args = shift;
 
-    bless $args, $class;
+    Furl->new(
+        timeout => $args->{timeout} || 5,
+        agent   => $args->{agent}   || __PACKAGE__."/$VERSION",
+    );
+}
+
+our %CACHE = ();
+
+sub expand {
+    my ($class, $url, $opt) = @_;
+
+    return $CACHE{$url} if exists $CACHE{$url};
+
+    my @urls;
+
+    $class->_expand($url, \@urls);
+
+    if ($opt->{cache}) {
+        $CACHE{$url} = $urls[-1];
+    }
+
+    return $urls[-1];
+}
+
+sub expand_all {
+    my ($class, $url, $opt) = @_;
+
+    return $CACHE{$url} if exists $CACHE{$url};
+
+    my @urls;
+
+    $class->_expand($url, \@urls);
+
+    if ($opt->{cache}) {
+        $CACHE{$url} = [@urls];
+    }
+
+    return @urls;
+}
+
+sub _expand {
+    my ($class, $url, $urls, $opt) = @_;
+
+    $opt->{ua} ||= _default_ua();
+
+    push @{$urls}, $url;
+
+    my $res = $opt->{ua}->request(
+        method        => 'HEAD',
+        url           => $url,
+        max_redirects => 0,
+        headers       => [ Connection => 'close' ],
+    );
+
+    if (my $next_url = $res->headers->header('location')) {
+        return $class->_expand($next_url, $urls, $opt);
+    }
 }
 
 1;
@@ -18,17 +74,48 @@ __END__
 
 =head1 NAME
 
-WWW::Expand::More - one line description
+WWW::Expand::More - The expander for shorten URL
 
 
 =head1 SYNOPSIS
 
     use WWW::Expand::More;
 
+    my $expanded_url  = WWW::Wxpand::More->expand('http://exsample.com/foo');
+
+    my @expanded_urls = WWW::Wxpand::More->expand_all('http://exsample.com/foo');
+
+    # options
+    print WWW::Wxpand::More->expand('http://exsample.com/foo' => {
+        timeout => 1,
+        agent   => 'YourUA/1.0',
+    });
+
 
 =head1 DESCRIPTION
 
-WWW::Expand::More is
+WWW::Expand::More is the expander for shorten URL.
+
+
+=head1 METHODS
+
+=head2 expand($url:Str, $opt:HashRef)
+
+=head2 expand_all($url:Str, $opt:HashRef)
+
+=head3 $opt: HashRef
+
+=head4 ua
+
+The User Agent Object that needs to have compatiblity with an interface of L<Furl>.
+
+=head4 timeout => $sec
+
+=head4 agent => $agent_name
+
+=head4 cache => boolean
+
+If you set true value on C<cache> option, then the expanded URL will cache.
 
 
 =head1 REPOSITORY
@@ -51,7 +138,9 @@ Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
-L<Other::Module>
+L<Furl>
+
+L<WWW::Expand>
 
 
 =head1 LICENSE
--- t/01_basic.t
+++ t/01_basic.t
@@ -4,8 +4,30 @@ use Test::More;
 
 use WWW::Expand::More;
 
-can_ok 'WWW::Expand::More', qw/new/;
+if ($ENV{AUTHOR_TEST} || $ENV{TRAVIS}) {
+    is(
+        WWW::Expand::More->expand('http://bit.ly/1BPj30x'),
+        'https://www.google.com/search?q=Perl',
+    );
 
-# write more tests
+    my @urls = WWW::Expand::More->expand_all('http://bit.ly/1BPj30x');
+    is $urls[0], 'http://bit.ly/1BPj30x';
+    is $urls[1], 'https://goo.gl/dXwzqw';
+    is $urls[2], 'https://www.google.com/search?q=Perl';
+
+    is scalar(keys %WWW::Expand::More::CACHE), 0;
+
+    is(
+        WWW::Expand::More->expand('https://goo.gl/dXwzqw', { cache => 1 }),
+        'https://www.google.com/search?q=Perl',
+    );
+
+    is(
+        $WWW::Expand::More::CACHE{'https://goo.gl/dXwzqw'},
+        'https://www.google.com/search?q=Perl',
+    );
+}
+
+ok 1;
 
 done_testing;
